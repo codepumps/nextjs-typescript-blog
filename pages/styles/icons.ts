0045453424c88669d72656aa671a7e46dd1d12ee
serkan <serkan_sayhan@hotmail.com>
2020-09-08 16:15:34 +0300

    update pages

--- pages/styles/icons.ts
+++
@@ -1,5 +0,0 @@
-export { Github } from "@styled-icons/evaicons-solid/Github";
-export { Facebook } from "@styled-icons/fa-brands/Facebook";
-export { LinkedinWithCircle } from "@styled-icons/entypo-social/LinkedinWithCircle";
-export { Twitter } from "@styled-icons/boxicons-logos/Twitter";
-export { TwitterWithCircle } from "@styled-icons/entypo-social/TwitterWithCircle";
